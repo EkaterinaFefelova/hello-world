1777fa10626ebde22dd2fd70e3276fe49981d0fe
EkaterinaFefelova <fefelova.ekaterina@gmail.com>
2024-07-09 17:19:58 +0300

    main branch

--- Lesson5/src/Main.java
+++ Lesson5/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Branch One, come on!!!");
+        System.out.println("Hello, Main branch!!!");
 
     }
 }
